d140c542361ae1fd7caab978b0473456721451d4
RajaDey1908 <raja.dey1908@gmail.com>
2020-03-02 00:34:58 +0530

    map setup done

--- src/screen/Maps/map.js
+++ src/screen/Maps/map.js
@@ -1,15 +1,72 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Button , StyleSheet} from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-const MapScreen = (props) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Map Screen</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => props.navigation.navigate('home')}
-      />
-    </View>
-  );
+// const MapScreen = (props) => {
+class MapScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: ''
+    }
+  }
+
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+      // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      //   <Text>Map Screen</Text>
+      //   <MapView
+      //     region={this.state.region}
+      //     // onRegionChange={this.onRegionChange}
+      //   />
+      //   {/* <Button
+      //     title="Go to Home"
+      //     onPress={() => props.navigation.navigate('home')}
+      //   /> */}
+      // </View>
+      // <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+        </MapView>
+      // </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    flex:1,
+     ...StyleSheet.absoluteFillObject,
+  },
+});
 export default MapScreen;
