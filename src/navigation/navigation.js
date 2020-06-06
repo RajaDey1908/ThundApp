e25867aee8e7a645ca4b1830f62d720586b78331
RajaDey1908 <raja.dey1908@gmail.com>
2020-06-07 00:31:20 +0530

    navigation partly complted

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import HomeScreen from '../screen/Home/Home'
 import DetailsScreen from '../screen/Details/Details'
 import SplashScreen from '../screen/Splash/Splash'
 import LoginScreen from '../screen/Login/Login'
 import MapScreen from '../screen/Maps/map'
 import ImageScreen from '../screen/Image/Image'
+import Tab1Screen from '../screen/Tab1/Tab1'
+import Tab2Screen from '../screen/Tab2/Tab2'
+import Drawer1Screen from '../screen/Drawer1/Drawer1'
+import Drawer2Screen from '../screen/Drawer2/Drawer2'
 
 
 const Stack = createStackNavigator();
-const Navigation = () => {
+const StackNav = () => {
     return(
-        <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen
                     name="splash"
@@ -45,9 +51,43 @@ const Navigation = () => {
                     options={{ title: 'Image', headerShown: false }}
                 />
             </Stack.Navigator>
-        </NavigationContainer>
     );
 }
 
+const SettingsStack = createStackNavigator();
 
-export default Navigation;
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Tab 1" component={Tab1Screen} />
+      <SettingsStack.Screen name="Tab 2" component={Tab2Screen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+
+const Drawer = createDrawerNavigator();
+function DrawerTabScreen() {
+  return (
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Drawer1Screen" component={Drawer1Screen} />
+        <Drawer.Screen name="Drawer2Screen" component={Drawer2Screen} />
+      </Drawer.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const TabNav= function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={StackNav} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+        <Tab.Screen name="Drawer" component={DrawerTabScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default TabNav;
---
+++ src/screen/Drawer1/Drawer1.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text} from 'react-native';
+
+
+const Drawer1Screen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Drawer 1 Screen</Text>
+    </View>
+  );
+}
+export default Drawer1Screen;
